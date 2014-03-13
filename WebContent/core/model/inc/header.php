409fe68c5f814af7bb899639a89105c5bf8fbd13
erlantzromero <erlantzromero@e8dac8df-953a-086d-1848-21c27e162ff5>
2014-03-13 12:17:17 +0000

    agregar,modificar completados falta arreglar head,index y por terminar el resto

---
+++ WebContent/core/model/inc/header.php
@@ -0,0 +1,11 @@
+<header class="header">
+	<div class="wrapper">
+		<?php if($seccion != 'index'){ 
+			include('inc/navBar.php');
+		} ?>
+		<div class="logo">
+			<span>Agenda</span>
+			<span>online</span>			
+		</div>
+	</div>
+</header>
