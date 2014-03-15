aac7fc72918792b7c5e60aa4cc02458a36107fe2
edu.monterrubio@gmail.com <edu.monterrubio@gmail.com@e8dac8df-953a-086d-1848-21c27e162ff5>
2014-03-15 17:53:45 +0000

    Primera importacion agenda web

--- src/com/ipartek/agenda/bean/Amigo.java
+++ src/com/ipartek/agenda/bean/Amigo.java
@@ -1,5 +1,10 @@
 package com.ipartek.agenda.bean;
 
 public class Amigo {
-
+    private int id;
+    private String nombre;
+    private String apellido
+    private String calle
+    private String localidad;
+    private int
 }
