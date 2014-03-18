3fee1bed9ca21d540ae136fbd2817df84d518763
patricianavascuesvega@gmail.com <patricianavascuesvega@gmail.com@e8dac8df-953a-086d-1848-21c27e162ff5>
2014-03-18 12:26:24 +0000

    Actualización de Agenda Online 		- añadir log4j para poder recoger los logs 		- Cambios en Util para preparar la búsuqeda.

--- branches/Patricia/src/main/com/ipartek/agenda/util/Util.java
+++ branches/Patricia/src/main/com/ipartek/agenda/util/Util.java
@@ -1,5 +1,30 @@
 package com.ipartek.agenda.util;
 
+import java.util.ArrayList;
+
+import com.ipartek.agenda.bean.Amigo;
+
+/**
+ * Clase util en donde se realizan las tareas necesarias para la búsuqeda de
+ * amigos.
+ * 
+ * @author Patricia Navascués
+ * @version 1.0
+ * 
+ */
 public class Util {
+
+	/**
+	 * Método para realizar la busqueda de amigos.
+	 * 
+	 * @param nombre
+	 *            nombre o fragmento de nombre que se va a buscar
+	 * 
+	 *            return listaAmigos
+	 */
+	public static ArrayList<Amigo> getAmigoBusqueda(String nombre) {
+
+		return new ArrayList<Amigo>();
+	}
 
 }
